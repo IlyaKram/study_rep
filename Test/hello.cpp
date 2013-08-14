b1812055e7c5f552e9d279e5f6d74d27b89c844d
IlyaKram <ikramichev@ikramichev-Lenovo-G780.(none)>
2013-08-14 17:18:12 +0400

    my commit

--- Test/hello.cpp
+++ Test/hello.cpp
@@ -1,7 +1,16 @@
 #include "hello.h"
 #include <iostream>
+
+const int& MyFunc()
+{
+	return 5;
+}
+
 int main()
 {
 	std::cout<<sHello<<"\n";
+	int i = MyFunc();
+	if (i == 5)
+		std::cout<<"5";
 	return 0;
 }
